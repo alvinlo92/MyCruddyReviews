8aaed33502268ebea576847876aa28fdedb17858
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2019-07-09 23:36:50 -0700

    Started working on CRUD

--- app/app.js
+++ app/app.js
@@ -1,110 +1,49 @@
-/*
-
- ### Basic Reqs
-- [ ] Where to store data? (localstorage)
-- [ ] How to modify data? (update action, delete action)
-
-*/
-
-//localStorage functions
 var createItem = function(key, value) {
-  return window.localStorage.setItem(key, value);
+    return window.localStorage.setItem(key, value);
 }
-
 var updateItem = function(key, value) {
-  return window.localStorage.setItem(key, value);
+    return window.localStorage.setItem(key, value);
 }
 
 var deleteItem = function(key) {
-  return window.localStorage.removeItem(key);
+    return window.localStorage.removeItem(key);
 }
 
 var clearDatabase = function() {
-  return window.localStorage.clear();
+    return window.localStorage.clear();
 }
 
-var showDatabaseContents = function() {
-  $('tbody').html('');
-
-  for (var i = 0; i < window.localStorage.length; i++) {
-    var key = window.localStorage.key(i);
-    $('tbody').append(`<tr><td>${key}</td><td>${window.localStorage.getItem(key)}</td></tr>`)
-  }
+var getNameInput = function() {
+    return $('.name').val();
 }
 
-var keyExists = function(key) {
-  return window.localStorage.getItem(key) !== null
+var getReviewInput = function() {
+    return $('.review').val();
 }
 
-var getKeyInput = function() {
-  return $('.key').val();
+var getRatingInput = function() {
+    return $('input:checked').val();
 }
 
-var getValueInput = function() {
-  return $('.value').val();
-}
-
-var resetInputs = function() {
-  $('.key').val('');
-  $('.value').val('');
+var resetInput = function() {
+    $('.name').val('');
+    $('.review').val('');
 }
 
 $(document).ready(function() {
-  showDatabaseContents();
+    $('.post').click(function() {
+        createItem(getNameInput(), getReviewInput());
+    });
 
-  $('.create').click(function() {
-    if (getKeyInput() !== '' && getValueInput() !== '') {
-      if (keyExists(getKeyInput())) {
-        if(confirm('key already exists in database, do you want to update instead?')) {
-          updateItem(getKeyInput(), getValueInput());
-          showDatabaseContents();
-        }
-      } else {
-        createItem(getKeyInput(), getValueInput());
-        showDatabaseContents();
-        resetInputs();
-      }
-    } else  {
-      alert('key and value must not be blank');
-    }
-  });
-
-  $('.update').click(function() {
-    if (getKeyInput() !== '' && getValueInput() !== '') {
-      if (keyExists(getKeyInput())) {
-        updateItem(getKeyInput(), getValueInput());
-        showDatabaseContents();
-        resetInputs();
-      } else {
-        alert('key does not exist in database');
-      }
-    } else {
-      alert('key and value must not be blank');
-    }
-  });
-
-  $('.delete').click(function() {
-     if (getKeyInput() !== '') {
-      if (keyExists(getKeyInput())) {
-        deleteItem(getKeyInput());
-        showDatabaseContents();
-        resetInputs();
-      } else {
-        alert('key does not exist in database');
-      }
-    } else {
-      alert('key must not be blank');
-    }
-  });
-
-  $('.reset').click(function() {
-    resetInputs();
-  })
-
-  $('.clear').click(function() {
-    if (confirm('WARNING: Are you sure you want to clear the database? \n                THIS ACTION CANNOT BE UNDONE')) {
-      clearDatabase();
-      showDatabaseContents();
-    }
-  })
+    $('.cancel').click(function() {
+        resetInput();
+    });
 })
+
+// post is create
+// cancel is delete
+// edit post is going to be update
+// clear database is remove all reviews
+
+// have generic resturant1, restaurant2... as key
+// use object as value
